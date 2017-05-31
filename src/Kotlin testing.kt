a3e511c8038109b809fedc2f3dee3e16c5b24e12
IronBlossom <i4idrhine@gmail.com>
2017-06-01 03:27:45 +0600

    >> Some Extension function practice >> Function with expression body >> Using function infix features. >> Tested that function parameters are always 'val' means you can not specify 'var' or 'val' on parameter cause its always 'val' >> Using IntRange cool feature (0..limit), oh!! and 'limit' is inclusive. >> Live template 'exfun' for 'Extension Function' :)

---
+++ src/Kotlin testing.kt
@@ -0,0 +1,24 @@
+/**
+ * Created by ironb on 6/1/2017.
+ */
+
+val loremIpsum="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+infix fun String.lipsum(limit:Int):String = loremIpsum.substring(0..(limit-1))
+
+infix fun String.shouldBeGeneratedBy(num: Int): String {
+    var n=num
+    val strBuilder=StringBuilder()
+   while (n>0){
+       strBuilder.append(this)
+       n--
+   }
+    return strBuilder.toString()
+}
+
+fun main(args: Array<String>) {
+println("" lipsum 100)
+
+    println("Rhine " shouldBeGeneratedBy 100)
+}
+
